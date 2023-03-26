e5eb0e7479b545ba8d75c8519e0b55a9a9c91373
Mike DeLong <mikedelong@outlook.com>
2023-03-26 15:48:44 -0400

    feature: first working version of code that uses the HF pipeline for a sentiment analysis task

---
+++ main.py
@@ -0,0 +1,45 @@
+"""
+Summarize text?
+"""
+
+from logging import FileHandler
+from logging import INFO
+from logging import StreamHandler
+from logging import basicConfig
+from logging import getLogger
+from pathlib import Path
+from sys import stdout
+
+from arrow import now
+from pandas import set_option
+from transformers import pipeline
+
+
+DATA_FOLDER = './data/'
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+LOG_FORMAT = '%(asctime)s.%(msecs)03d - %(levelname)s - %(name)s - %(message)s'
+LOG_PATH = Path('./logs/')
+
+if __name__ == '__main__':
+    time_start = now()
+    LOG_PATH.mkdir(exist_ok=True)
+
+    set_option('display.max_colwidth', None)  # was -1 and caused a warning
+    run_start_time = now().strftime('%Y-%m-%d_%H-%M-%S')
+    file_root_name = 'find_property'
+    LOGFILE = '{}/log-{}-{}.log'.format(LOG_PATH, run_start_time, file_root_name)
+
+    handlers = [FileHandler(LOGFILE), StreamHandler(stdout)]
+    # noinspection PyArgumentList
+    basicConfig(datefmt=DATE_FORMAT, format=LOG_FORMAT, handlers=handlers, level=INFO, )
+
+    logger = getLogger()
+    logger.info('started')
+
+    task = 'sentiment-analysis'
+    model = 'distilbert-base-uncased-finetuned-sst-2-english'
+    classifier = pipeline(model=model, task=task)
+    classifier_result =  classifier('We are very happy to introduce pipeline to the transformers repository.')
+    logger.info(classifier_result)
+
+    logger.info('total time: {:5.2f}s'.format((now() - time_start).total_seconds()))
