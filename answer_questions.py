30e545e993787793585911b7c63bed71d5649982
Mike DeLong <mikedelong@outlook.com>
2023-04-24 14:15:43 -0400

    feature: boilerplate code and logging

---
+++ answer_questions.py
@@ -0,0 +1,37 @@
+"""
+Summarize text?
+"""
+
+from logging import FileHandler
+from logging import INFO
+from logging import StreamHandler
+from logging import basicConfig
+from logging import getLogger
+from pathlib import Path
+from sys import stdout
+
+from arrow import now
+from pandas import set_option
+
+DATA_FOLDER = './data/'
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+LOG_FORMAT = '%(asctime)s.%(msecs)03d - %(levelname)s - %(name)s - %(message)s'
+LOG_PATH = Path('./logs/')
+
+if __name__ == '__main__':
+    time_start = now()
+    LOG_PATH.mkdir(exist_ok=True)
+
+    set_option('display.max_colwidth', None)  # was -1 and caused a warning
+    run_start_time = now().strftime('%Y-%m-%d_%H-%M-%S')
+    file_root_name = 'answer_questions'
+    LOGFILE = '{}/log-{}-{}.log'.format(LOG_PATH, run_start_time, file_root_name)
+
+    handlers = [FileHandler(LOGFILE), StreamHandler(stdout)]
+    # noinspection PyArgumentList
+    basicConfig(datefmt=DATE_FORMAT, format=LOG_FORMAT, handlers=handlers, level=INFO, )
+
+    logger = getLogger()
+    logger.info('started')
+
+    logger.info('total time: {:5.2f}s'.format((now() - time_start).total_seconds()))
