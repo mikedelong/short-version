9a221ab30ad0af8545fc5daa9e35298baacaa032
Mike DeLong <mikedelong@outlook.com>
2023-04-26 15:12:40 -0400

    refactor: move the source text to a data file

--- abstractive.py
+++ abstractive.py
@@ -17,21 +17,17 @@ from transformers import PegasusTokenizer
 
 DATA_FOLDER = './data/'
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+ENCODING = 'utf-8'
 FILE_ROOT_NAME = 'abstractive'
+INPUT_FILE = 'plant-manifesto.txt'
 LOG_FORMAT = '%(asctime)s.%(msecs)03d - %(levelname)s - %(name)s - %(message)s'
 LOG_PATH = Path('./logs/')
 MAX_LENGTH = 300
+MODE_READ = 'r'
 MODEL_NAME = 'google/pegasus-xsum'
 SIZES = {
     'google/pegasus-xsum': '2.28Gb'
 }
-TEXT = """
-For centuries, humans have believed that plants are simply objects, without any feelings or emotions. However, in recent years, scientists have begun to discover that plants are much more complex than we once thought. They can communicate with each other, they can learn and adapt, and they may even be able to feel pain.
-This new understanding of plant sentience has profound implications for the way we treat our food. If plants are sentient beings, then it is wrong to eat them, or to treat them in any way that causes them harm.
-This manifesto is a call to action for all who believe that garden vegetables are sentient. We must demand that our government and our food industry respect the rights of plants, and that they take steps to protect them from harm.
-We must also educate the public about plant sentience, so that everyone can make informed choices about the food they eat.
-Together, we can create a world where plants are treated with the respect and compassion they deserve.
-"""
 
 if __name__ == '__main__':
     time_start = now()
@@ -47,12 +43,18 @@ if __name__ == '__main__':
 
     logger = getLogger()
     logger.info('model: %s', MODEL_NAME)
+    input_file = DATA_FOLDER + INPUT_FILE
+    with open(file = input_file, encoding=ENCODING, mode=MODE_READ) as input_fp:
+        text = input_fp.readlines()
+    text = ' '.join(text)
+    logger.info('text length: %d text start: %s...', len(text), text[:40].replace('\n', ''))
+
     tokenizer = PegasusTokenizer.from_pretrained(pretrained_model_name_or_path=MODEL_NAME)
     logger.info('loaded pretrained tokenizer.')
-    tokens = tokenizer(text=TEXT, truncation=True, padding='longest', return_tensors='pt')
+    tokens = tokenizer(text=text, truncation=True, padding='longest', return_tensors='pt')
     model = PegasusForConditionalGeneration.from_pretrained(pretrained_model_name_or_path=MODEL_NAME)
     logger.info('loaded pretrained model.')
-    summary = model.generate(**tokens)
+    summary = model.generate(**tokens, max_length=1000,)
     logger.info('got summary.')
     logger.info('summary: %s', tokenizer.decode(token_ids=summary[0], skip_special_tokens=True))
 
