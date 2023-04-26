e72baf8add4ada339715a6f9a45b4ba1137675da
Mike DeLong <mikedelong@outlook.com>
2023-04-26 15:25:36 -0400

    feature: loop through all the text samples in our data folder

--- abstractive.py
+++ abstractive.py
@@ -14,12 +14,12 @@ from arrow import now
 from pandas import set_option
 from transformers import PegasusForConditionalGeneration
 from transformers import PegasusTokenizer
+from glob import glob
 
 DATA_FOLDER = './data/'
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 ENCODING = 'utf-8'
 FILE_ROOT_NAME = 'abstractive'
-INPUT_FILE = 'plant-manifesto.txt'
 LOG_FORMAT = '%(asctime)s.%(msecs)03d - %(levelname)s - %(name)s - %(message)s'
 LOG_PATH = Path('./logs/')
 MAX_LENGTH = 300
@@ -43,20 +43,18 @@ if __name__ == '__main__':
 
     logger = getLogger()
     logger.info('model: %s', MODEL_NAME)
-    input_file = DATA_FOLDER + INPUT_FILE
-    logger.info('input file: %s', input_file)
-    with open(file = input_file, encoding=ENCODING, mode=MODE_READ) as input_fp:
-        text = input_fp.readlines()
-    text = ' '.join(text)
-    logger.info('text length: %d text start: %s...', len(text), text[:40].replace('\n', ''))
-
-    tokenizer = PegasusTokenizer.from_pretrained(pretrained_model_name_or_path=MODEL_NAME)
-    logger.info('loaded pretrained tokenizer.')
-    tokens = tokenizer(text=text, truncation=True, padding='longest', return_tensors='pt')
     model = PegasusForConditionalGeneration.from_pretrained(pretrained_model_name_or_path=MODEL_NAME)
     logger.info('loaded pretrained model.')
-    summary = model.generate(**tokens, max_length=1000,)
-    logger.info('got summary.')
-    logger.info('summary: %s', tokenizer.decode(token_ids=summary[0], skip_special_tokens=True))
+    tokenizer = PegasusTokenizer.from_pretrained(pretrained_model_name_or_path=MODEL_NAME)
+    logger.info('loaded pretrained tokenizer.')
+    for input_file in glob(DATA_FOLDER + '*.txt'):
+        logger.info('input file: %s', input_file)
+        with open(file = input_file, encoding=ENCODING, mode=MODE_READ) as input_fp:
+            text = input_fp.readlines()
+        text = ' '.join(text)
+        logger.info('text length: %d text start: %s...', len(text), text[:40].replace('\n', ''))
+        tokens = tokenizer(text=text, truncation=True, padding='longest', return_tensors='pt')
+        summary = model.generate(**tokens, max_length=MAX_LENGTH,)
+        logger.info('summary: %s', tokenizer.decode(token_ids=summary[0], skip_special_tokens=True))
 
     logger.info('total time: {:5.2f}s'.format((now() - time_start).total_seconds()))
