a9a42f04e131b7c4ed354c845f90d0433b763a6c
Mike DeLong <mikedelong@outlook.com>
2023-04-27 13:51:28 -0400

    feature: loop through the available models and run all the text fragments

--- abstractive.py
+++ abstractive.py
@@ -24,7 +24,9 @@ LOG_FORMAT = '%(asctime)s.%(msecs)03d - %(levelname)s - %(name)s - %(message)s'
 LOG_PATH = Path('./logs/')
 MAX_LENGTH = 300
 MODE_READ = 'r'
-MODEL_NAME = 'google/pegasus-xsum'
+MODEL_NAMES = [
+    'google/pegasus-xsum',
+    ]
 SIZES = {
     'google/pegasus-xsum': '2.28Gb'
 }
@@ -42,19 +44,20 @@ if __name__ == '__main__':
     basicConfig(datefmt=DATE_FORMAT, format=LOG_FORMAT, handlers=handlers, level=INFO, )
 
     logger = getLogger()
-    logger.info('model: %s', MODEL_NAME)
-    model = PegasusForConditionalGeneration.from_pretrained(pretrained_model_name_or_path=MODEL_NAME)
-    logger.info('loaded pretrained model.')
-    tokenizer = PegasusTokenizer.from_pretrained(pretrained_model_name_or_path=MODEL_NAME)
-    logger.info('loaded pretrained tokenizer.')
-    for input_file in glob(DATA_FOLDER + '*.txt'):
-        logger.info('input file: %s', input_file)
-        with open(file=input_file, encoding=ENCODING, mode=MODE_READ) as input_fp:
-            text = input_fp.readlines()
-        text = ' '.join(text)
-        logger.info('text length: %d text start: %s...', len(text), text[:40].replace('\n', ''))
-        tokens = tokenizer(text=text, truncation=True, padding='longest', return_tensors='pt')
-        summary = model.generate(**tokens, max_length=MAX_LENGTH, )
-        logger.info('summary: %s', tokenizer.decode(token_ids=summary[0], skip_special_tokens=True))
+    for model_name in MODEL_NAMES:
+        logger.info('model: %s', model_name)
+        model = PegasusForConditionalGeneration.from_pretrained(pretrained_model_name_or_path=model_name)
+        logger.info('loaded pretrained model.')
+        tokenizer = PegasusTokenizer.from_pretrained(pretrained_model_name_or_path=model_name)
+        logger.info('loaded pretrained tokenizer.')
+        for input_file in glob(DATA_FOLDER + '*.txt'):
+            logger.info('input file: %s', input_file)
+            with open(file=input_file, encoding=ENCODING, mode=MODE_READ) as input_fp:
+                text = input_fp.readlines()
+            text = ' '.join(text)
+            logger.info('text length: %d text start: %s...', len(text), text[:40].replace('\n', ''))
+            tokens = tokenizer(text=text, truncation=True, padding='longest', return_tensors='pt')
+            summary = model.generate(**tokens, max_length=MAX_LENGTH, )
+            logger.info('summary: %s', tokenizer.decode(token_ids=summary[0], skip_special_tokens=True))
 
     logger.info('total time: {:5.2f}s'.format((now() - time_start).total_seconds()))
