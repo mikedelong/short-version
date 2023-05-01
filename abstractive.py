1f132f199db3a0f188ec5998c3db414804417e76
Mike DeLong <mikedelong@outlook.com>
2023-05-01 14:13:47 -0400

    feature: only do cases we haven't already done, a first cut

--- abstractive.py
+++ abstractive.py
@@ -19,6 +19,8 @@ from pandas import set_option
 from transformers import PegasusForConditionalGeneration
 from transformers import PegasusTokenizer
 
+from pandas import concat
+
 DATA_FOLDER = './data/'
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 ENCODING = 'utf-8'
@@ -65,28 +67,34 @@ if __name__ == '__main__':
     # TODO only generate cases we haven't already generated
     input_files = list(glob(DATA_FOLDER + '*.txt'))
     for model_name in MODEL_NAMES:
-        logger.info('model: %s', model_name)
-        model = PegasusForConditionalGeneration.from_pretrained(pretrained_model_name_or_path=model_name)
-        logger.info('loaded pretrained model.')
-        tokenizer = PegasusTokenizer.from_pretrained(pretrained_model_name_or_path=model_name)
-        logger.info('loaded pretrained tokenizer.')
-        for input_file in input_files:
-            logger.info('input file: %s', input_file)
-            with open(file=input_file, encoding=ENCODING, mode=MODE_READ) as input_fp:
-                text = input_fp.readlines()
-            text = ' '.join(text)
-            logger.info('text length: %d text start: %s...', len(text), text[:40].replace('\n', ''))
-            tokens = tokenizer(text=text, truncation=True, padding='longest', return_tensors='pt')
-            summary = model.generate(**tokens, max_length=MAX_LENGTH, )
-            summary_text = tokenizer.decode(token_ids=summary[0], skip_special_tokens=True)
-            logger.info('summary length: %d summary text: %s', len(summary_text), summary_text)
-            OUTPUT_MODEL_NAMES.append(model_name)
-            OUTPUT_FILE_NAMES.append(input_file)
-            OUTPUT_SUMMARIES.append(summary_text)
-            result_filename = RESULT_FOLDER + RESULT_FILE
-            logger.info('writing: %s', result_filename)
-            # TODO concat the results and write the updated results
-            result_df = DataFrame(data={'model': OUTPUT_MODEL_NAMES, 'file': OUTPUT_FILE_NAMES, 'summary': OUTPUT_SUMMARIES})
-            result_df.to_csv(index=False, path_or_buf=result_filename, )
+        model_df = prior_df[prior_df['model'] == model_name]
+        files = model_df['file'].values
+        done_files = {name for name in files if name in input_files}
+        done = len(input_files) == len(done_files)
+        if not done:
+            logger.info('model: %s', model_name)
+            model = PegasusForConditionalGeneration.from_pretrained(pretrained_model_name_or_path=model_name)
+            logger.info('loaded pretrained model.')
+            tokenizer = PegasusTokenizer.from_pretrained(pretrained_model_name_or_path=model_name)
+            logger.info('loaded pretrained tokenizer.')
+            for input_file in input_files:
+                logger.info('input file: %s', input_file)
+                with open(file=input_file, encoding=ENCODING, mode=MODE_READ) as input_fp:
+                    text = input_fp.readlines()
+                text = ' '.join(text)
+                logger.info('text length: %d text start: %s...', len(text), text[:40].replace('\n', ''))
+                tokens = tokenizer(text=text, truncation=True, padding='longest', return_tensors='pt')
+                summary = model.generate(**tokens, max_length=MAX_LENGTH, )
+                summary_text = tokenizer.decode(token_ids=summary[0], skip_special_tokens=True)
+                logger.info('summary length: %d summary text: %s', len(summary_text), summary_text)
+                OUTPUT_MODEL_NAMES.append(model_name)
+                OUTPUT_FILE_NAMES.append(input_file)
+                OUTPUT_SUMMARIES.append(summary_text)
+                result_filename = RESULT_FOLDER + RESULT_FILE
+                logger.info('writing: %s', result_filename)
+                # TODO concat the results and write the updated results
+                result_df = DataFrame(data={'model': OUTPUT_MODEL_NAMES, 'file': OUTPUT_FILE_NAMES, 'summary': OUTPUT_SUMMARIES})
+                result_df = concat([prior_df, result_df])
+                result_df.to_csv(index=False, path_or_buf=result_filename, )
 
     logger.info('total time: {:5.2f}s'.format((now() - time_start).total_seconds()))
